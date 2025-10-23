6b78799886ded35ba042a5b82f0fd8372fee5701
vjr <233987190+jeyaramanv2006@users.noreply.github.com>
2025-10-23 17:55:36 +0000

    Create a random number generator

--- cpp/src/kissrandom.h
+++ cpp/src/kissrandom.h
@@ -1,1 +1,37 @@
+#pragma once
 
+template<typename T = unsigned long long>
+class kissrand {
+    T x,y,z,c;
+public:
+    kissrand(T seed = 1234567890987654321ULL) {
+        x = seed;
+        y = 362436362436362436ULL;
+        z = 1066149217761810ULL;
+        c = 123456123456123456ULL;
+    }
+
+    T kiss() {
+        z = 6906969069ULL * z + 1234567;
+        y ^= (y << 13); 
+        y ^= (y >> 17);
+        y ^= (y << 43);
+
+        T t = (x << 58) + c;
+        c = (x >> 6);
+        x += t;
+        c += (x < t);
+
+        return x + y + z;
+    }
+
+    int flip() { 
+        return kiss() & 1; 
+    }
+    int index(int n) { 
+        return kiss() % n; 
+    }
+    void set_seed(T seed) { 
+        x = seed; 
+    }
+};
